2b04d7091e2064abc13857d5c3e6d37e8abc9cb9
mauricio-largel <143734542+mauricio-largel@users.noreply.github.com>
2025-10-13 15:35:38 -0300

    Update gender.view.php

--- gender.view.php
+++ gender.view.php
@@ -1,16 +1,1 @@
-<?php
 
-class GenderView {
-
-    function showGender($genres) {
-        require_once 'templates/layouts/header.phtml';
-        require_once 'templates/gender.phtml';
-        require_once 'templates/layouts/footer.phtml';
-    }
-
-    function showGenderFilms($filmsByGender) {
-        require_once 'templates/layouts/header.phtml';
-        require_once 'templates/genderFilms.phtml';
-        require_once 'templates/layouts/footer.phtml';
-    }
-}
